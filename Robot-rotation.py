feee573c53847643f44d0a7e6082b639228228a8
Lino Rehan <liorian045@gmail.com>
2026-03-12 06:26:05 +0200

    y,z axis added

--- Robot-rotation.py
+++ Robot-rotation.py
@@ -5,6 +5,25 @@ def rotation_matrix_x(degrees):
 [0, np.cos(angle), -np.sin(angle)],
  [0 ,np.sin(angle), np.cos(angle)]]
 )
+def rotation_matrix_y(degrees):
+    angle=np.radians(degrees)
+    return np.array([[np.cos(angle),0 ,np.sin(angle)],
+                     [0 , 1 , 0],
+                     [np.sin(angle), 0, np.cos(angle)]
 
-result=rotation_matrix_x(45)
-print(result)
+                     ])
+
+def rotation_matrix_z(degrees):
+ angle=np.radians(degrees)
+ return np.array([
+     [np.cos(angle),-np.sin(angle),0],
+     [np.sin(angle),np.cos(angle),0],
+     [0 ,0 ,1]
+
+ ])
+result_x=rotation_matrix_x(45)
+result_y=rotation_matrix_y(60)
+result_z=rotation_matrix_z(30)
+print(result_x)
+print(result_y)
+print(result_z)
